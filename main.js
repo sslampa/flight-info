3d9e0ec23f972d6deab7880cc1fb2f09d79659f5
Suzan Sucro <suzansucro@gmail.com>
2017-09-03 15:29:35 -0700

    NOOOO CORS againgit add -A!

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ function searchForEmployee( formName )
 {
   //this is where to perform validation
   var uInput = document.getElementById('name');
-  if ( !(uInput.value).length )
+  if ( !(uInput.value).length ) {
     console.log("Please enter search input.");
     return;
-  } elseif ( !localStorage.getItem(uInput.value)) {
-    getData();
+  } else if ( !localStorage.getItem(uInput.value) ) {
+    getThatData();
   } else { return localStorage.getItem(uInput.value) }
+}
 
-
+function getThatData( origin ) {
+  if (window.XMLHttpRequest) {
+    httpRequest = new XMLHttpRequest();
+  }
+  httpRequest.onreadystatechange = function() {
+    console.log("it worked")
+  }
+  httpRequest.open('POST', )
 }
